7483ebc8fa8124f3a9a3c0318ed47d42c03c01b8
InacioAntonio <inaciodu@outlook.com>
2023-12-12 20:29:51 -0300

    Terminando classe DAO produto_bode e corrigindo erros em Bode_Produto Relatorio

--- src/aplicacao/Relatorio_BodeProd.java
+++ src/aplicacao/Relatorio_BodeProd.java
@@ -5,28 +5,30 @@ public class Relatorio_BodeProd extends BodeREL{
 	private float preco;
 	private float pesoProduto;
 	private String categoria;
-	private String descricao;
+	//private String descricao;
 	private int quantidade;
 	//Bode
-	private String cpfFazendeiro;
-	private float pesoBode;
+	private int Id;
 	private String nome;
-	private String genero;
+
 	
-	public Relatorio_BodeProd(float preco, float pesoProduto, String categoria, String descricao, int quantidade,
-			String cpfFazendeiro, float pesoBode, String nome, String genero) {
+	public Relatorio_BodeProd(float preco, float pesoProduto, String categoria, int quantidade, int id ) {
 		super();
 		this.preco = preco;
 		this.pesoProduto = pesoProduto;
 		this.categoria = categoria;
-		this.descricao = descricao;
+		this.Id = id;
 		this.quantidade = quantidade;
-		this.cpfFazendeiro = cpfFazendeiro;
-		this.pesoBode = pesoBode;
-		this.nome = nome;
-		this.genero = genero;
 	}
 	
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
 	public float getPreco() {
 		return preco;
 	}
@@ -45,41 +47,21 @@ public class Relatorio_BodeProd extends BodeREL{
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+
 	public int getQuantidade() {
 		return this.quantidade;
 	}
 	public void setQuantidade(int quantidade) {
 		this.quantidade = quantidade;
 	}
-	public String getCpfFazendeiro() {
-		return cpfFazendeiro;
-	}
-	public void setCpfFazendeiro(String cpfFazendeiro) {
-		this.cpfFazendeiro = cpfFazendeiro;
-	}
-	public float getPesoBode() {
-		return pesoBode;
-	}
-	public void setPesoBode(float pesoBode) {
-		this.pesoBode = pesoBode;
-	}
+	
+	
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getGenero() {
-		return genero;
-	}
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
+	
 	
 }
--- src/persistencia/Bode_produtoDAO.java
+++ src/persistencia/Bode_produtoDAO.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Bode_produtoDAO {
 	private String cadastra = "INSERT INTO Bode_produto (id_bode, id_produto) VALUES (?,?);";
 	private String buscar = "SELECT * FROM Bode_produto WHERE id_bode=?";
-	private String relatorio_geral ="SELECT * FROM relatorio_geral_de_Bodes_Produto";
+	private String relatorio_geral ="SELECT * FROM relatorio_geral_de_Bodes_Produto WHERE cpf_fazendeiro = ?";
 	private String relatorio_associados ="SELECT * FROM relatorio_geral_valores  WHERE cpf_fazendeiro = ?";
 	private String deletarProduto = "DELETE FROM Bode_produto WHERE id_produto=?";
 	private String deletarBode = "DELETE FROM Bode_produto WHERE id_bode=?";
@@ -20,24 +20,50 @@ public class Bode_produtoDAO {
 	public Bode_produtoDAO() {
 		con = new Conexao("postgres","1234","jdbc:postgresql://localhost:5432/BDbode");
 	}
-	
+	public ArrayList<Relatorio_BodeProd> relatorioGeral(String cpfFazendeiro){
+		ArrayList<Relatorio_BodeProd> lista = null;
+		FazendeiroDAO fd = new FazendeiroDAO();
+		Fazendeiro teste = fd.buscar(cpfFazendeiro);
+		if (teste != null) {
+			try {
+				con.conectar();
+				PreparedStatement instrucao = con.getConexao().prepareStatement(relatorio_associados);
+				instrucao.setString(1,cpfFazendeiro);
+				ResultSet rs = instrucao.executeQuery();
+				lista = new ArrayList<Relatorio_BodeProd>();
+				while(rs.next()) {
+					lista.add(new Relatorio_BodeProd(rs.getFloat("preco"), rs.getFloat("peso_produto"), rs.getString("categoria"),rs.getInt("quantidade"),rs.getInt("id")));
+				}
+				con.desconectar();
+			}catch(Exception e) {
+				System.out.println(e.toString());
+			}
+			
+			
+		}
+		return lista;
+	}
 	public ArrayList<Relatorio_BodeProd> relatorioAssociados(String cpfFazendeiro) {
 		ArrayList<Relatorio_BodeProd> lista = null;
-		
-		try {
+		FazendeiroDAO fd = new FazendeiroDAO();
+		Fazendeiro teste = fd.buscar(cpfFazendeiro);
+		if (teste != null) {
+			try {
 			con.conectar();
 			PreparedStatement instrucao = con.getConexao().prepareStatement(relatorio_associados);
 			instrucao.setString(1,cpfFazendeiro);
 			ResultSet rs = instrucao.executeQuery();
 			lista = new ArrayList<Relatorio_BodeProd>();
 			while(rs.next()) {
-				lista.add(new Relatorio_BodeProd(rs.getFloat("preco"), rs.getFloat("peso"), atualizarBode, atualizarBode, 0, atualizarBode, 0, atualizarBode, atualizarBode));
+				lista.add(new Relatorio_BodeProd(rs.getFloat("preco"), rs.getFloat("peso_produto"), rs.getString("categoria"),rs.getInt("quantidade"),rs.getInt("id")));
 			}
 			con.desconectar();
 		}catch(Exception e) {
 			System.out.println(e.toString());
 		}
 		
+		
+		}
 		return lista;
 	}
 	
@@ -60,7 +86,7 @@ public class Bode_produtoDAO {
 			con.conectar();
 			PreparedStatement instrucao = con.getConexao().prepareStatement(deletarProduto);
 			instrucao.setInt(1, id_produto);
-			instrucao.executeQuery();
+			instrucao.execute();
 			con.desconectar();
 		}catch(Exception e) {
 			System.out.println(e.toString());
@@ -72,7 +98,7 @@ public class Bode_produtoDAO {
 			con.conectar();
 			PreparedStatement instrucao = con.getConexao().prepareStatement(deletarBode);
 			instrucao.setInt(1, id_bode);
-			instrucao.executeQuery();
+			instrucao.execute();
 			con.desconectar();
 		}catch(Exception e) {
 			System.out.println(e.toString());
